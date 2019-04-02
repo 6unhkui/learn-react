a36602f507529d13eff0a1690a8d54c865aee499
siminkyung <poo0112@naver.com>
2019-04-02 17:04:54 +0900

    Updates

---
+++ precache-manifest.d12123e2ea4e2441140a2d94b7eb9b73.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "63e951cbfce500ff6ed3",
+    "url": "/react-study/tree/master/Mobx-State-Tree/mst-ajax/static/js/runtime~main.1aacf4b5.js"
+  },
+  {
+    "revision": "84afe6040add4a88614d",
+    "url": "/react-study/tree/master/Mobx-State-Tree/mst-ajax/static/js/main.444b9647.chunk.js"
+  },
+  {
+    "revision": "5d37b9cf21989da625f6",
+    "url": "/react-study/tree/master/Mobx-State-Tree/mst-ajax/static/js/2.b8da5519.chunk.js"
+  },
+  {
+    "revision": "84afe6040add4a88614d",
+    "url": "/react-study/tree/master/Mobx-State-Tree/mst-ajax/static/css/main.874a1759.chunk.css"
+  },
+  {
+    "revision": "6485e6233ec9d2e341eb2c141d1c3130",
+    "url": "/react-study/tree/master/Mobx-State-Tree/mst-ajax/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/react-study/tree/master/Mobx-State-Tree/mst-ajax/precache-manifest.d12123e2ea4e2441140a2d94b7eb9b73.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/react-study/tree/master/Mobx-State-Tree/mst-ajax/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
