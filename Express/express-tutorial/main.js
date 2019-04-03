36575d720875cf9802f7300d6f3c9c505e0169a7
siminkyung <poo0112@naver.com>
2019-04-03 18:11:00 +0900

    [Express] 스터디

--- Express/express-tutorial/main.js
+++ Express/express-tutorial/main.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var app = express();
 var user = require('./routes/user');
+var morgan = require('morgan');
+var bodyParser  = require('body-parser');
 
 /*
-기본 라우팅
+@ 기본 라우팅
 app.MATHOD(PATH, HANDLER)
 
 MATHOD : http 요청 메소드 - get, post, delete, put ...
@@ -11,8 +13,25 @@ PATH : 라우트 경로
 HANDLER : 실행될 콜백 함수
 */
 
+/*
+@ 미들웨어 생성 후 추가
+var myLogger = function(req, res, next) {
+    console.log(req.url);
+    next();
+}
+
+app.use(myLogger);
+*/
+
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+
+//1과 2의 path는 동일하지만 먼저 실행되는 함수가 우선권을 지님
+//1 
+app.use('/', express.static('public'));
+//2
 app.get('/', function(req, res) { // '/'=루트 경로=index.js에 get 요청이 들어오면 콜백 함수 실행
-    res.send('Hello world'); 
+    res.send('Hello world');
 });
 
 app.use('/user', user);
--- Express/express-tutorial/routes/user.js
+++ Express/express-tutorial/routes/user.js
@@ -5,10 +5,14 @@ router.get('/:id', function(req, res) { // :id = parameter / 동적인 값을 �
     res.send('Receved a GET request, param ' + req.params.id);
 });
 
-router.post('/', function(req, res) { 
-    res.json({ success : true });
+router.post('/', function(req, res) {
+    console.log(JSON.stringify(req.body, null, 2));
+    res.json({
+        success : true,
+        user : req.body.username
+    });
 });
-
+ 
 router.put('/', function(req, res) {
     res.status(400).json({ message: 'Hey,you. Bad Request!' });
 });
